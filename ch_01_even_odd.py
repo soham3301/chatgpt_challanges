4e8762ba4f3683fcbd23665de71305b668cb470e
Soham Datta <splint3r777@gmail.com>
2026-06-23 22:26:41 +0530

    added more 2

---
+++ ch_01_even_odd.py
@@ -0,0 +1,6 @@
+number = int(input("Enter your number here.\n"))
+
+if (number % 2 == 0):
+    print("This is an EVEN number")
+else:
+    print("This is an ODD number")
---
+++ ch_02_age_category.py
@@ -0,0 +1,12 @@
+#0-12. 13-19, 20-59, 60+
+
+age = int(input("Enter your age here: \n"))
+
+if (age < 13):
+    print("You are a CHILD")
+elif (age < 20):
+    print("You are a TEENAGER")
+elif (age < 60):
+    print("You are an ADULT")
+else:
+    print("You are SENIOR CITIZEN")
