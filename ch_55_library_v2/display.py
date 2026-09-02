da6cb43e7143e473019b2d35e30ca961f91fe584
soham3301 <splint3r777@gmail.com>
2026-09-02 16:33:32 +0530

    small test

---
+++ ch_55_library_v2/display.py
@@ -0,0 +1,10 @@
+
+class Display:
+    def __init__(self):
+        self.name = "Display"
+
+    def welcome_screen(self):
+        print("Welcome to the Library")
+
+    def show_readymade_data(self, data):
+        print(data)
